fbf531ee126710eaef9bfc86ae1c5a37720d6783
Mateusz-PC <mpomagier@gmail.com>
2024-06-06 01:06:14 +0200

    Add remove books and no books indicator

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,59 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div id="app">
+
+    <!-- heading -->
+    <header>
+      <h1>Books<span>.app</span></h1>
+    </header>
+
+    <!-- books list -->
+    <ul>
+      <li
+          :key="index"
+          v-for="(book, index) in books">
+            {{ book.title }}, {{ book.price }}
+            <button @click="removeBook(index)">Remove</button>
+        </li>
+    </ul>
+
+    <!-- no books message -->
+    <p v-if="!books.length">No books...</p>
+
+    <!-- add book form -->
+    <form>
+      <label>
+        Title:
+        <input type="text" name="title">
+      </label>
+      <label>
+        price:
+        <input type="number" name="price">
+      </label>
+      <button>Add book</button>
+    </form>
+
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
   name: 'App',
-  components: {
-    HelloWorld
+  data: () => ({
+    books: [
+      {
+        title: 'The Catcher in the Rye',
+        price: 20
+      },
+      {
+        title: 'Of Mice and Men',
+        price: 18
+      }
+    ]
+  }),
+  methods: {
+    removeBook (index) {
+      this.books.splice(index, 1)
+    }
   }
 }
 </script>
-
-<style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
